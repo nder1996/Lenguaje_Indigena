d0e7abc84b2f8161c48fdea853c4cb0768275a68
Ander_AAM <ander_2030@hotmail.com>
2022-11-04 21:05:43 +0000

    Maqueta terminada

--- src/components/home_abecedario.vue
+++ src/components/home_abecedario.vue
@@ -8,10 +8,15 @@
         transition="dialog-bottom-transition"
       >
         <template v-slot:activator="{ on, attrs }">
-          <v-btn color="blue-grey darken-1" dark v-bind="attrs" v-on="on"> Abecedario </v-btn>
+          <v-btn color="blue-grey darken-1" dark v-bind="attrs" v-on="on">
+            Abecedario
+          </v-btn>
         </template>
         <v-card>
-          <v-toolbar dark color="primary">
+          <v-toolbar
+            dark
+            style="background-color: rgb(205, 151, 84, 0.9) !important"
+          >
             <v-btn icon dark @click="dialog = false">
               <v-icon>mdi-close</v-icon>
             </v-btn>
@@ -21,66 +26,36 @@
               <v-btn dark text @click="dialog = false"> Save </v-btn>
             </v-toolbar-items>
           </v-toolbar>
-          <v-list three-line subheader>
-            <v-subheader>User Controls</v-subheader>
-            <v-list-item>
-              <v-list-item-content>
-                <v-list-item-title>Content filtering</v-list-item-title>
-                <v-list-item-subtitle
-                  >Set the content filtering level to restrict apps that can be
-                  downloaded</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-content>
-                <v-list-item-title>Password</v-list-item-title>
-                <v-list-item-subtitle
-                  >Require password for purchase or use password to restrict
-                  purchase</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-          <v-divider></v-divider>
-          <v-list three-line subheader>
-            <v-subheader>General</v-subheader>
-            <v-list-item>
-              <v-list-item-action>
-                <v-checkbox v-model="notifications"></v-checkbox>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>Notifications</v-list-item-title>
-                <v-list-item-subtitle
-                  >Notify me about updates to apps or games that I
-                  downloaded</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-action>
-                <v-checkbox v-model="sound"></v-checkbox>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>Sound</v-list-item-title>
-                <v-list-item-subtitle
-                  >Auto-update apps at any time. Data charges may
-                  apply</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-action>
-                <v-checkbox v-model="widgets"></v-checkbox>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>Auto-add widgets</v-list-item-title>
-                <v-list-item-subtitle
-                  >Automatically add home screen widgets</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
+          <v-card-text class="mt-10">
+            <v-row>
+              <v-col
+                v-for="i in 5"
+                :key="i"
+                cols="12"
+                sm="4"
+                md="4"
+                align="center"
+                jusitfy="center"
+              >
+                <v-card elevation="2" width="300">
+                  <v-img
+                    height="150"
+                    src="https://media.istockphoto.com/vectors/capital-letters-in-geometric-indigenous-style-for-hipster-theme-vector-id962818048"
+                  ></v-img>
+                  <v-card-title>Abecedario idioma indigena</v-card-title>
+                  <v-card-subtitle class="text-start"
+                    >Abecedario idioma español</v-card-subtitle
+                  >
+                  <audio
+                    class="mt-3 mb-3"
+                    src="https://lenguasdecolombia.caroycuervo.gov.co/ICCadmin/ICC/documentos/audio/poemas%20fredy%20chikangana_0.mp3"
+                    controls
+                    style="width: 90%"
+                  ></audio>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-card-text>
         </v-card>
       </v-dialog>
     </v-row>
@@ -92,9 +67,6 @@ export default {
   data() {
     return {
       dialog: false,
-      notifications: false,
-      sound: true,
-      widgets: false,
     };
   },
 };
--- src/components/home_colores.vue
+++ src/components/home_colores.vue
@@ -13,76 +13,49 @@
           </v-btn>
         </template>
         <v-card>
-          <v-toolbar dark color="primary">
+          <v-toolbar
+            dark
+            style="background-color: rgb(205, 151, 84, 0.9) !important"
+          >
             <v-btn icon dark @click="dialog = false">
               <v-icon>mdi-close</v-icon>
             </v-btn>
-            <v-toolbar-title>Settings</v-toolbar-title>
+            <v-toolbar-title>Colores</v-toolbar-title>
             <v-spacer></v-spacer>
             <v-toolbar-items>
               <v-btn dark text @click="dialog = false"> Save </v-btn>
             </v-toolbar-items>
           </v-toolbar>
-          <v-list three-line subheader>
-            <v-subheader>User Controls</v-subheader>
-            <v-list-item>
-              <v-list-item-content>
-                <v-list-item-title>Content filtering</v-list-item-title>
-                <v-list-item-subtitle
-                  >Set the content filtering level to restrict apps that can be
-                  downloaded</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-content>
-                <v-list-item-title>Password</v-list-item-title>
-                <v-list-item-subtitle
-                  >Require password for purchase or use password to restrict
-                  purchase</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-          <v-divider></v-divider>
-          <v-list three-line subheader>
-            <v-subheader>General</v-subheader>
-            <v-list-item>
-              <v-list-item-action>
-                <v-checkbox v-model="notifications"></v-checkbox>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>Notifications</v-list-item-title>
-                <v-list-item-subtitle
-                  >Notify me about updates to apps or games that I
-                  downloaded</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-action>
-                <v-checkbox v-model="sound"></v-checkbox>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>Sound</v-list-item-title>
-                <v-list-item-subtitle
-                  >Auto-update apps at any time. Data charges may
-                  apply</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-action>
-                <v-checkbox v-model="widgets"></v-checkbox>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>Auto-add widgets</v-list-item-title>
-                <v-list-item-subtitle
-                  >Automatically add home screen widgets</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
+          <v-card-text class="mt-10">
+            <v-row>
+              <v-col
+                v-for="i in 5"
+                :key="i"
+                cols="12"
+                sm="4"
+                md="4"
+                align="center"
+                jusitfy="center"
+              >
+                <v-card elevation="2" width="300">
+                  <v-img
+                    height="150"
+                    src="https://share.america.gov/wp-content/uploads/2017/11/B0B669-1.jpg"
+                  ></v-img>
+                  <v-card-title>Colores idioma indigena</v-card-title>
+                  <v-card-subtitle class="text-start"
+                    >Colores idioma español</v-card-subtitle
+                  >
+                  <audio
+                    class="mt-3 mb-3"
+                    src="https://lenguasdecolombia.caroycuervo.gov.co/ICCadmin/ICC/documentos/audio/Cancion_Helena_waunana.mp3"
+                    controls
+                    style="width: 90%"
+                  ></audio>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-card-text>
         </v-card>
       </v-dialog>
     </v-row>
